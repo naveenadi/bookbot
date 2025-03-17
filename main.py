702ee145ec9dfc3959bae7afa622f1a107eec869
Aditya Agarwal <naveenagarwal123451@gmail.com>
2025-03-18 02:21:40 +0530

    update main.py file

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import sys
 from pathlib import Path
-
-def words_count(file_contents):
-    words = file_contents.split()
-    print(len(words))
+from stats import get_num_words
 
 def chars_count(file_contents):
     contents = file_contents.lower()
@@ -26,24 +23,29 @@ def sort_on(dict):
     return dict["times"]
 
 def main() -> int:
-    path_to_file = Path("books/frankenstein.txt")
-    
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_book = Path(sys.argv[1])
+
     try:
-        with open(path_to_file) as f:
+        with open(path_to_book) as f:
             file_contents = f.read()
             print("--- Begin report of books/frankenstein.txt ---")
-            print("{} words found in the document\n".format(words_count(file_contents)))
+            print("{} words found in the document\n".format(get_num_words(file_contents)))
             chars_dict_count = chars_count(file_contents)
             letters_list = dict_to_dictlist(chars_dict_count)
             letters_list.sort(key=sort_on, reverse=True)
             for dict in letters_list:
-                print("The '{}' character was found {} times".format(dict["letter"], dict["times"]))
+                #print("The '{}' character was found {} times".format(dict["letter"], dict["times"]))
+                print(f"{dict["letter"]}: {dict["times"]}")
             print("--- End report ---")
     except FileNotFoundError:
-        print(f"Error: Could not find file {path_to_file}")
+        print(f"Error: Could not find file {path_to_book}")
         return 1
     except IOError as e:
-        print(f"Error reading file {path_to_file}: {e}")
+        print(f"Error reading file {path_to_book}: {e}")
         return 1
         
     return 0
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(file_contents):
+    words = file_contents.split()
+    return len(words)
